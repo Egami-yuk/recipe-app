4b1908f2a7cc2067ea1e8e32a846f2b83f207dd6
Egami-yuk <e.y062097@gmail.com>
2020-11-26 18:07:27 +0900

    ユーザーページ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @recipes = @user.recipes.order("created_at DESC")
     @ranking = Recipe.find(Favorite.group(:recipe_id).order('count(recipe_id) desc').limit(3).pluck(:recipe_id))
-    @favorite = Favorite.where(user_id: @user.id)
-    # @favorite = Recipe.where(id: favorite.recipe_id)
-    # .limit(3).order("created_at DESC")
+    @favorite = Favorite.where(user_id: @user.id).order("created_at DESC").limit(3)
   end
 end
